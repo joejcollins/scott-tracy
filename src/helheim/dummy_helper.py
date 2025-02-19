7ddd6578423829d1cd7ca1048ef7df6eb9c69ab0
Joe J Collins <joejcollins@gmail.com>
2025-02-19 14:54:02 +0000

    feat: Implement dummy addition functionality

---
+++ src/helheim/dummy_helper.py
@@ -0,0 +1,7 @@
+"""Dummy helpers."""
+
+
+def add_things(a: int, b: int) -> int:
+    """Do some addition."""
+    the_thing = a + b
+    return the_thing
--- tests/dummy_helper_test.py
+++ tests/dummy_helper_test.py
@@ -3,6 +3,15 @@
 from helheim import dummy_helper
 
 
+def test_dummy_addition() -> None:
+    """Confirm the dummy addition function behaves as expected."""
+    # Arrange
+    the_first_number = 2
+    the_second_number = 3
+    # Act
+    the_final_number = dummy_helper.add_things(the_first_number, the_second_number)
+    # Assert
+    assert the_final_number == 5
 
 
 def test_addition():
